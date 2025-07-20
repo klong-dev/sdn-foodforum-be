b99b4387a4a759c7a314ee9a97d533e26c758df4
DuyenNguyen <duyenntbse183629@fpt.edu.vn>
2025-07-20 18:20:30 +0700

    update post-related models

---
+++ src/models/category.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    slug: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    imageUrl: {
+        type: String,
+        trim: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+        set: function () {
+            return Date.now();
+        }
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Category', categorySchema)
+
